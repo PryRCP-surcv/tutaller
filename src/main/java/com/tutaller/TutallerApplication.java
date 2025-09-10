b5c92660a4d8972e5175332814d210913ba0107f
Surich Caso <surichcv@gmail.com>
2025-09-09 20:41:03 -0500

    Primer commit

---
+++ src/main/java/com/tutaller/TutallerApplication.java
@@ -0,0 +1,13 @@
+package com.tutaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutaller/TutallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
